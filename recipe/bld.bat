34b45e02f2d5e8a010e1df5870daf86499d290ec
Anton Tetov <anton@tetov.se>
2023-09-21 14:12:18 +0200

    nicer formatting of build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,20 +3,25 @@
 mkdir build
 cd build
 
-cmake %SRC_DIR%                                                   ^
+cmake %CMAKE_ARGS% -LAH                                           ^
       -G "Visual Studio 17 2022"                                  ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"              ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"                 ^
       -DOPENVDB_BUILD_PYTHON_MODULE=ON                            ^
       -DOPENVDB_CORE_SHARED=ON                                    ^
       -DOPENVDB_CORE_STATIC=OFF                                   ^
-      -DPYOPENVDB_INSTALL_DIRECTORY=%PREFIX%/Lib/site-packages    ^
+      -DPYOPENVDB_INSTALL_DIRECTORY="%PREFIX%/Lib/site-packages"  ^
+      -DPython_EXECUTABLE="%PYTHON%"                              ^
       -DUSE_EXPLICIT_INSTANTIATION=OFF                            ^
       -DUSE_NUMPY=ON                                              ^
-      -DPython_EXECUTABLE=%PYTHON%                                ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"              ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"
+      ..
 
 if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
 
-cmake --build . --verbose --target install --config Release
+cmake --build .                                                   ^
+      --verbose                                                   ^
+      --parallel "%CPU_COUNT%"                                    ^
+      --target install                                            ^
+      --config Release
 
 if %ERRORLEVEL% neq 0 exit 1
