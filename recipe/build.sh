e9b92de22e2a3432b5deb7a04ae5fce61f8e651c
Anton Tetov <anton@tetov.se>
2024-11-06 15:14:34 +0100

    nanobind_DIR and python dep in build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,18 +5,19 @@ set -ex
 mkdir -p build
 cd build
 
-cmake ${CMAKE_ARGS} -LAH                \
-      -G "Unix Makefiles"               \
-      -DOPENVDB_BUILD_BINARIES=ON       \
-      -DOPENVDB_BUILD_PYTHON_MODULE=ON  \
-      -DOPENVDB_BUILD_VDB_VIEW=ON       \
-      -DOPENVDB_CORE_SHARED=ON          \
-      -DOPENVDB_CORE_STATIC=OFF         \
-      -DUSE_EXPLICIT_INSTANTIATION=OFF  \
-      -DUSE_NUMPY=ON                    \
+cmake ${CMAKE_ARGS} -LAH                    \
+      -G "Unix Makefiles"                   \
+      -Dnanobind_DIR=$SP_DIR/nanobind/cmake \
+      -DOPENVDB_BUILD_BINARIES=ON           \
+      -DOPENVDB_BUILD_PYTHON_MODULE=ON      \
+      -DOPENVDB_BUILD_VDB_VIEW=ON           \
+      -DOPENVDB_CORE_SHARED=ON              \
+      -DOPENVDB_CORE_STATIC=OFF             \
+      -DUSE_EXPLICIT_INSTANTIATION=OFF      \
+      -DUSE_NUMPY=ON                        \
       ..
 
-cmake --build .                         \
-      --verbose                         \
-      --parallel "${CPU_COUNT}"         \
+cmake --build .                             \
+      --verbose                             \
+      --parallel "${CPU_COUNT}"             \
       --target install
