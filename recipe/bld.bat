cf8fc354a11fddff32adf0b925f0c92ec1e3ac06
Anton Tetov <anton@tetov.se>
2024-11-06 17:45:03 +0100

    updates

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,9 +3,6 @@
 mkdir build
 cd build
 
-:: see https://github.com/boostorg/system/issues/32#issuecomment-462912013
-set "CXXFLAGS=%CXXFLAGS% /DHAVE_SNPRINTF=1"
-
 cmake %CMAKE_ARGS% -LAH                                           ^
       -G "Visual Studio 17 2022"                                  ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"              ^
@@ -16,7 +13,7 @@ cmake %CMAKE_ARGS% -LAH                                           ^
       -DOPENVDB_BUILD_VDB_VIEW=ON                                 ^
       -DOPENVDB_CORE_SHARED=ON                                    ^
       -DOPENVDB_CORE_STATIC=OFF                                   ^
-      -DPYOPENVDB_INSTALL_DIRECTORY="%PREFIX%/Lib/site-packages"  ^
+      -DVDB_PYTHON_INSTALL_DIRECTORY="%PREFIX%/Lib/site-packages" ^
       -DPython_EXECUTABLE="%PYTHON%"                              ^
       -DUSE_EXPLICIT_INSTANTIATION=OFF                            ^
       -DUSE_NUMPY=ON                                              ^
